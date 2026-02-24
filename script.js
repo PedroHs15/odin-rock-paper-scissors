7e7886697d01c5bc86dbaa4212e8076a365011dd
PedroHs15 <pedrosilvadz15@gmail.com>
2026-02-24 14:36:50 -0300

    refatoração do código
    Mudança de nomes nas variáveis globais, criação de
    variáveis locais e remoção de parâmetros; ajuste na
    função playRound caso humanChoice seja null;
    incrementação de placar separado da string template
    e remoção dos returns console.logs.

--- script.js
+++ script.js
@@ -1,84 +1,95 @@
-const maoUm = 'Pedra';
-const maoDois = 'Papel';
-const maoTres = 'Tesoura';
+const PEDRA = 'Pedra';
+const PAPEL = 'Papel';
+const TESOURA = 'Tesoura';
 
 let humanScore = 0;
 let computerScore = 0;
 
 function capitalize(palavra) {
-    palavra = palavra.toLowerCase();
-    return palavra.charAt(0).toUpperCase() + palavra.slice(1);
+    if (!palavra){
+        return null;
+    }
+    const palavraMinuscula = palavra.toLowerCase();
+    return palavraMinuscula.charAt(0).toUpperCase() + palavraMinuscula.slice(1);
 }
 
-function getComputerChoice(escolha) {
-    escolha = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    const escolha = Math.floor(Math.random() * 3);
     if (escolha === 0) {
-        return escolha = maoUm;
+        return PEDRA;
     } 
     else if (escolha === 1) {
-        return escolha = maoDois;
+        return PAPEL;
     } 
     else {
-        return escolha = maoTres;
+        return TESOURA;
     }
 }
 
-function getHumanChoice(input) {
-    input = prompt('Escolha uma opção entre: Pedra, Papel e Tesoura');
+function getHumanChoice() {
+    let input = prompt('Escolha uma opção entre: Pedra, Papel e Tesoura');
     input = capitalize(input);
     if (input === 'Pedra') {
-        return maoUm;
+        return PEDRA;
     } 
     else if (input === 'Papel') {
-        return maoDois;
+        return PAPEL;
     } 
     else if (input === 'Tesoura'){
-        return maoTres;
+        return TESOURA;
     }
     else {
-        return 'Digite uma opção válida por favor';
+        return null;
     }
 }
 
-
-function playRound(humanChoice, computerChoice) {
-    console.log(humanChoice = getHumanChoice());
-    console.log(computerChoice = getComputerChoice());
+// execute: playRound(getHumanChoice(), getComputerChoice())
+function playRound(humanChoice, computerChoice) { 
+    if (humanChoice === null) {
+        return 'Digite uma opção válida por favor';
+    }
+    console.log(`${humanChoice} x ${computerChoice}`);
     if (humanChoice === computerChoice) {
         console.log('Resultado: Empate');
-        return console.log(`Placar: Você ${humanScore} x ${computerScore} Computador`);
+        return `Placar: Você ${humanScore} x ${computerScore} Computador`;
     }
 
     if (humanChoice === 'Pedra') {
         if (computerChoice === 'Tesoura') {
             console.log('Resultado: Você ganhou!');
-            return console.log(`Placar: Você ${(++humanScore)} x ${computerScore} Computador`);
+            ++humanScore;
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
         else {
             console.log('Resultado: Você perdeu, tente novamente.');
-            return console.log(`Placar: Você ${humanScore} x ${(++computerScore)} Computador`);
+            ++computerScore;
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
     }
 
     if (humanChoice === 'Papel') {
         if (computerChoice === 'Pedra') {
             console.log('Resultado: Você ganhou!');
-            return console.log(`Placar: Você ${(++humanScore)} x ${computerScore} Computador`);
+            ++humanScore;
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
         else {
             console.log('Resultado: Você perdeu, tente novamente.');
-            return console.log(`Placar: Você ${humanScore} x ${(++computerScore)} Computador`);
+            ++computerScore;
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
     }
 
     if (humanChoice === 'Tesoura') {
         if (computerChoice === 'Papel') {
-            console.log('Resultado: Você ganhou!');            
-            return console.log(`Placar: Você ${(++humanScore)} x ${computerScore} Computador`);
+            console.log('Resultado: Você ganhou!');
+            ++humanScore;            
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
         else {
             console.log('Resultado: Você perdeu, tente novamente.');
-            return console.log(`Placar: Você ${humanScore} x ${(++computerScore)} Computador`);
+            ++computerScore;
+            return `Placar: Você ${humanScore} x ${computerScore} Computador`;
         }
     }
 }
